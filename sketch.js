4d6b26c7470036dca83dee65d8294723f8a11eef
KlaudioGjoni <89693995+KlaudioGjoni@users.noreply.github.com>
2021-08-28 14:27:22 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,44 @@
+var box;
+
+
+
+
+
+
+function setup() {
+  createCanvas(400,400);
+
+  box = createSprite (200,200,30,30);
+
+
+
+}
+
+function draw() 
+{
+  background(30);
+
+if (KeyIsDown(RIGHT_ARROW)){
+  box.position.x = box.position.x +5;
+
+}
+
+if (KeyIsDown(LEFT_ARROW)){
+  box.position.x = box.position.x -5;
+  
+}
+
+if (KeyIsDown(UP_ARROW)){
+  box.position.y = box.position.y -5;
+}
+
+if (KeyIsDown(DOWN_ARROW)){
+ box.position.y = box.position.y +5;
+    
+}
+  drawSprites ()
+}
+
+
+
+
